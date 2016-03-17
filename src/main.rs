6ca654dfaa21b00096c7bd74eb5ac4d16a7e451a
Bryan Mytko <bryanmytko@gmail.com>
2016-03-16 22:24:27 -0400

    Add basic request

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,13 @@
+extern crate hyper;
+
+mod request;
+use request::*;
+
 fn main() {
+    // @TODO get url params from CLI
+    let test_uri = "http://www.google.com";
+    let mut request = Request::new();
+
+    println!("Request: {:?}", test_uri);
+    println!("Response: {:?}", request.get(test_uri));
 }
