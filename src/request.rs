ff83a174b8cd2fd9cc5f55b30307b6d4d0e2c7f3
Bryan Mytko <bryanmytko@gmail.com>
2016-03-16 22:24:42 -0400

    Add Request module

---
+++ src/request.rs
@@ -0,0 +1,28 @@
+use hyper::Client;
+use hyper::header::Connection;
+use std::io::Read;
+
+pub struct Request {
+    response: String
+}
+
+impl Request {
+    pub fn new() -> Request {
+        Request {
+            response: "".to_owned(),
+        }
+    }
+
+    pub fn get<'a>(&mut self, uri: &'a str) -> String {
+        let client = Client::new();
+
+        let mut res = client.get(uri)
+            .header(Connection::close())
+            .send().unwrap();
+
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        self.response = body.clone();
+        body
+    }
+}
